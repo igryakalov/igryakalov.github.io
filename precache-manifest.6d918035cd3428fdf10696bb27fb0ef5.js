8329cf58c09ccc5462868aa2c6ae85d5a9d70662
igryakalov <48382996+igryakalov@users.noreply.github.com>
2019-05-06 21:46:56 +0300

    Add files via upload

---
+++ precache-manifest.6d918035cd3428fdf10696bb27fb0ef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/test/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.2160d828.js"
+  },
+  {
+    "revision": "069e53ae90c5fced58d2",
+    "url": "/test/static/js/main.069e53ae.chunk.js"
+  },
+  {
+    "revision": "1b0445640ce82da4865e",
+    "url": "/test/static/js/2.1b044564.chunk.js"
+  },
+  {
+    "revision": "069e53ae90c5fced58d2",
+    "url": "/test/static/css/main.3f5fd157.chunk.css"
+  },
+  {
+    "revision": "1b0445640ce82da4865e",
+    "url": "/test/static/css/2.e62c4ca8.chunk.css"
+  },
+  {
+    "revision": "2bb405dfee19fca42f998c041fb77931",
+    "url": "/test/index.html"
+  }
+];
