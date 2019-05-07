0fd9e2a9a96e87f41cf58f5ef35dc388026c3b74
igryakalov <48382996+igryakalov@users.noreply.github.com>
2019-05-07 11:31:31 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b94ff2ac8cbce5ed304b34dd6eea5f7.js"
+  "/precache-manifest.f27848cfa569a58be3abef70b0f51447.js"
 );
 
 workbox.clientsClaim();
