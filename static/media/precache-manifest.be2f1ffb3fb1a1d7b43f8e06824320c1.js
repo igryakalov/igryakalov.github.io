e08b51623ea9f283caf53c383162bc6105a04021
igryakalov <48382996+igryakalov@users.noreply.github.com>
2019-09-23 15:10:07 +0300

    Add files via upload

---
+++ static/media/precache-manifest.be2f1ffb3fb1a1d7b43f8e06824320c1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "c8769fd90f32e12533e0",
+    "url": "/static/css/main.893e6efe.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "668f92e936e6c426400802fee4c711b9",
+    "url": "/static/media/camera_200.668f92e9.png"
+  },
+  {
+    "revision": "1edd4619519394198e3e",
+    "url": "/static/js/2.18c574fe.chunk.js"
+  },
+  {
+    "revision": "c8769fd90f32e12533e0",
+    "url": "/static/js/main.525e33f6.chunk.js"
+  },
+  {
+    "revision": "04a801cd904c8728bd63cff0156e88f4",
+    "url": "/static/media/1.04a801cd.png"
+  },
+  {
+    "revision": "23af9aec9498448eddac35a60661d4b3",
+    "url": "/static/media/2.23af9aec.png"
+  },
+  {
+    "revision": "b4629ec2601f2a51443a09965bebe965",
+    "url": "/static/media/3.b4629ec2.png"
+  },
+  {
+    "revision": "1edd4619519394198e3e",
+    "url": "/static/css/2.ee0d9651.chunk.css"
+  },
+  {
+    "revision": "c3a17cfe151a52d20eba35e335e404ca",
+    "url": "/index.html"
+  }
+];
